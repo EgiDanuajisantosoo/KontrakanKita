1ed2e03567b5f5a1653ffd7bc12ffb04d0db67ba
EgiDanuajisantosoo <oneeeback@gmail.com>
2025-03-26 20:02:09 +0700

    melanjutkan dan fiks bug

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    
+    server: {
+      host: '192.168.1.6',
+      port: 3000,
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }),
+      tailwindcss(),
     ],
-});
+  });
+
